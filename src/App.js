505fff5c8385b5ed65706731bd43187ee7eae7ae
35kartal35 <mozdemir3545@gmail.com>
2023-02-10 22:41:41 +0300

    listCats

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from "react";
-import "./assets/App.css"
+import React, { useEffect } from "react";
+
 import HomePage from "./Page/HomePage";
 
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-
+import { useDispatch,useSelector } from "react-redux";
+import api from "./api/api"
+import urls from "./api/urls"
+import ActionTypes from "./redux/Action/ActionTypes";
 
 function App() {
+  const dispatch = useDispatch();
+  const { catsState, shacksState} = useSelector((state) => state);
+  
+  useEffect(()=>{
+    dispatch({type: ActionTypes.catsAction.GET_CAT_START});
+    api.get(urls.Cats)
+    .then((res)=>{
+      dispatch({type:ActionTypes.catsAction.GET_CAT_SUCCESS,
+      payload: res.data,
+      });
+    })
+    .catch((err)=> {
+      dispatch({
+        type:ActionTypes.catsAction.GET_CAT_FAİL,
+        payload: "Serverda bir hata oluştu",
+      });
+    });
+    dispatch({type:ActionTypes.shacksAction.GET_SHACK_START});
+    api.get(urls.Shacks)
+    .then((res)=>{
+      dispatch({
+        type:ActionTypes.shacksAction.GET_SHACK_SUCCESS,
+        payload:res.data,
+      });
+    })
+    .catch((err)=>{
+      dispatch({
+        type:ActionTypes.shacksAction.GET_SHACK_FAİL,
+        payload:"Serverda bir hata oluştu"
+      });
+    });
+  }, []);
+
+  if (catsState.success === false || shacksState.success === false)
+  return null;
+  
   return (
     <BrowserRouter>
     <Routes>
--- src/component/ListCats.js
+++ src/component/ListCats.js
@@ -1,17 +1,20 @@
+
 import React from "react";
+import { useDispatch,useSelector } from "react-redux";
 
 
 const ListCats=()=>{
     
     
+    const { catsState, shackState } = useSelector((state) => state);
 
+    console.log(catsState,shackState)
     
-
     return(
         <div classname="my-5">
-             <h1>Kediler</h1>
+             <h1>KEDİLERİN</h1>
         </div>
     )
 }
 
-export default ListCats
+export default ListCats;
