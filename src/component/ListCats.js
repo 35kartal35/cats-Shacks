682a86474dd8ce8eed2be3eb1a59cc53666087a0
35kartal35 <mozdemir3545@gmail.com>
2023-02-19 17:13:54 +0300

    search-bar

--- src/component/ListCats.js
+++ src/component/ListCats.js
@@ -1,5 +1,5 @@
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import "../assets/Listcats.css";
 import api from "../api/api"
@@ -14,9 +14,19 @@ const ListCats = () => {
 
   const dispatch = useDispatch()
   const { catsState } = useSelector((state) => state);
-  
-  
-  
+  const [searchCat, setSearchCat] = useState("");
+  const [filteredTekir, setFilteredTekir] = useState(catsState.cats)
+  useEffect(() => {
+    console.log(searchCat)
+    const temp = catsState.cats.filter(
+      (item) =>
+        item.name.toLowerCase().includes(searchCat.toLowerCase()) === true ||
+        item.color.toLowerCase().includes(searchCat.toLowerCase()) === true
+        );
+    setFilteredTekir(temp);
+  }, [searchCat]);
+
+
   const deletecats = (id) => {
     dispatch({ type: ActionTypes.catsAction.DELETE_CAT_START });
     api.delete(`${urls.Cats}/${id}`)
@@ -24,9 +34,9 @@ const ListCats = () => {
         dispatch({
           type: ActionTypes.catsAction.DELETE_CAT_SUCCESS,
           payload: id,
-          });
+        });
       },
-      window.location.reload())
+        window.location.reload())
 
       .catch(err => {
         dispatch({
@@ -38,28 +48,37 @@ const ListCats = () => {
 
   return (
     <div classname="container my-5">
+     
       <table classname="liste">
-        
+      <input
+          className="form-control w-100"
+          type="text"
+          placeholder="Aramak istediğiniz kedinin adını yazınız..."
+          value={searchCat}
+          onChange={(event) => setSearchCat(event.target.value)}
+
+        />
         <tbody>
-          {catsState.cats.map((cat, index) => {
-            
+        
+          {filteredTekir.map((cat, index) => {
+
             return (
               <div className="container my-5">
-              <div className="liste" >
-                <tr key={cat.id}>
-                  <th scope="row"><h1>{index + 1}</h1></th>
-                  <td><h2>{cat.name}</h2></td>
-                  <td><h2>{cat.age}</h2></td>
-                  <td><h2>{cat.color}</h2></td>
-                  
-                  <td>
-                    <Link className="btn btn-primary" to={`/cat-detay/${cat.id}`}>Detay</Link>
-                    <button onClick={() =>    deletecats(cat.id)} 
-                    className="btn btn-danger">Sil</button>
-                   <Link className="btn btn-success" to={`/cat-edit/${cat.id}`}>Düzenle</Link>
-                  </td>
-                </tr>
-              </div>
+                <div className="liste" >
+                  <tr key={cat.id}>
+                    <th scope="row"><h1>{index + 1}</h1></th>
+                    <td><h2>{cat.name}</h2></td>
+                    <td><h2>{cat.age}</h2></td>
+                    <td><h2>{cat.color}</h2></td>
+
+                    <td>
+                      <Link className="btn btn-primary" to={`/cat-detay/${cat.id}`}>Detay</Link>
+                      <button onClick={() => deletecats(cat.id)}
+                        className="btn btn-danger">Sil</button>
+                      <Link className="btn btn-success" to={`/cat-edit/${cat.id}`}>Düzenle</Link>
+                    </td>
+                  </tr>
+                </div>
               </div>
 
             )
@@ -69,8 +88,8 @@ const ListCats = () => {
 
         </tbody>
       </table>
-      
-     
+
+
     </div>
   )
 }
